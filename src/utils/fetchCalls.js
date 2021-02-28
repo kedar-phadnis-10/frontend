b107e9158ca5c6211a1d9d49f9c8d855b830ce26
Santhosh00009 <santoshjarupla9@gmail.com>
2021-02-28 20:33:57 +0530

    small chnages

--- src/utils/fetchCalls.js
+++ src/utils/fetchCalls.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios");
 
 const BASE_URL="http://localhost:5000";
 
-async function getColleges(){
+export const getColleges = async()=>{
     // const response = await fetch(`${BASE_URL}/colleges`, {
 	// 	method: "get",
 	// 	credentials: "include"
@@ -13,9 +13,7 @@ async function getColleges(){
 }
 
 
-async function getCollegeDetails(id){
+export const getCollegeDetails=async(id)=>{
     const response = await axios.get(`${BASE_URL}/colleges/${id}`);
     return response.data;
 }
-
-module.exports = { getColleges, getCollegeDetails}
